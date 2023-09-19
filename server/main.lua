f1007753c5b426ed1f737974897faee70da6ba90
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-19 08:56:33 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,7 @@
 --[[ ===================================================== ]]--
 
 local QBCore = exports['qb-core']:GetCoreObject()
+local debug = false        -- true only for debug.
 --
 local useCashAsItem = true -- true if you want to use cash as item
 local cashItem = "cash"    -- the cash item for the inventory
@@ -11,25 +12,41 @@ local cashItem = "cash"    -- the cash item for the inventory
 local useItemBox = true    -- true if you want to use the itembox popup 
 local useAddBox = true     -- true if you want to see the add itembox popup (only works if useItemBox = true)
 local useRemoveBox = false -- true if you want to see the remove itembox popup (only works if useItemBox = true)
+--
 
+--- Inventory ItemBox Popup
+---@param amount int
+---@param action string
 local function ItemBox(amount, action)
     if useItemBox and (useAddBox or useRemoveBox) then
         TriggerClientEvent('inventory:client:ItemBox', player.PlayerData.source, QBCore.Shared.Items[cashItem:lower()], action, amount)
     end
 end
 
+--- Get Player Cash
+---@param player object
 local function GetMoney(player)
     return player.Functions.GetMoney('cash')
 end
 
+--- Add Player Cash
+---@param player object
+---@param amount int
 local function AddMoney(player, amount)
     return player.Functions.AddMoney("cash", amount, nil)
 end
 
+--- Remove Player Cash
+---@param player object
+---@param amount int
 local function RemoveMoney(player, amount)
     return player.Functions.RemoveMoney("cash", amount, nil)
 end
 
+---Add Cash Item
+---@param player object
+---@param amount int
+---@param slot int
 local function AddItem(player, amount, slot)
     if slot ~= nil or slot ~= 0 then
         player.Functions.AddItem(cashItem:lower(), amount, slot)
@@ -39,10 +56,16 @@ local function AddItem(player, amount, slot)
     ItemBox(amount, "add")
 end
 
+---Remove Cash Item
+---@param player object
+---@param amount int
+---@param slot int
 local function RemoveItem(player, amount, slot)
     return player.Functions.RemoveItem(cashItem:lower(), amount, slot)
 end
 
+---Update Cash Item
+---@param id int
 local function UpdateCashItem(id)
     local player = QBCore.Functions.GetPlayer(id)
     if player and useCashAsItem then
@@ -67,14 +90,14 @@ end
 
 RegisterNetEvent('mh-cashasitem:server:updateCash', function(id, item, amount, action)
     local player = QBCore.Functions.GetPlayer(id)
-    if player and useCashAsItem then 
+    if player and useCashAsItem then
         if item and item.name == cashItem then
-	    if action == "add" then
-		AddMoney(player, amount, nil)
-	    elseif action == "remove" then
-		RemoveMoney(player, amount, nil)
-	    end
-	end
+            if action == "add" then
+                AddMoney(player, amount, nil)
+            elseif action == "remove" then
+                RemoveMoney(player, amount, nil)
+            end
+        end
     end
 end)
 
