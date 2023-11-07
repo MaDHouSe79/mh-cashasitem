3bb4f1bb309f9bb26e98268f8f2e2c4a74e73ee1
MaDHouSe <madhouse1979@gmail.com>
2023-11-07 07:52:04 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,8 +3,10 @@
 --[[ ===================================================== ]] --
 Config = {}
 
-Config.useCashAsItem = true -- true if you want to use cash as item
-Config.cashItem = "cash" -- the cash item for the inventory
+-- inventory itembox popup when you add or remove items.
+Config.useItemBox = false -- true if you want to use the itembox popup 
+Config.useAddBox = false -- true if you want to see the add itembox popup (only works if useItemBox = true)
+Config.useRemoveBox = false -- true if you want to see the remove itembox popup (only works if useItemBox = true)
 
 Config.CashItems = {
     ["cash"] = true,
@@ -12,8 +14,3 @@ Config.CashItems = {
 }
 
 Config.BlackmoneyCommand = "blackmoney" -- the command to see your blackmoney, same as cash or bank
-
--- inventory itembox popup when you add or remove items.
-Config.useItemBox = false -- true if you want to use the itembox popup 
-Config.useAddBox = false -- true if you want to see the add itembox popup (only works if useItemBox = true)
-Config.useRemoveBox = false -- true if you want to see the remove itembox popup (only works if useItemBox = true)
