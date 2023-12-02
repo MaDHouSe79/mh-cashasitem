02e57b9f6a1f3c4b42e19855528db07e7162b1a6
MaDHouSe <madhouse1979@gmail.com>
2023-12-02 13:59:43 +0100

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -1,27 +1,25 @@
 --[[ ===================================================== ]]--
 --[[           MH Cash As Item Script by MaDHouSe          ]]--
 --[[ ===================================================== ]]--
-
 local script = GetCurrentResourceName()
 
 local function checkVersion(err, responseText, headers)
-    curVersion = LoadResourceFile(script, "version")
+    local curVersion = LoadResourceFile(script, "version")
+    local version = string.gsub(curVersion, "%s+", "")
     if responseText == nil then
-        print("[^6"..script.."^7] Check for script update ^1FAILED^7")
+        print("[^1"..script.."^7] Check for script update ^1FAILED^7")
         return
     end
-    if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-        updateavail = true
-        print("\n^1----------------------------------------------------------------------------------^7")
-        print(script.." is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!\nupdate from https://github.com/MaDHouSe79/"..script.."")
+    if version ~= responseText and tonumber(version) < tonumber(responseText) then
         print("^1----------------------------------------------------------------------------------^7")
-    elseif tonumber(curVersion) > tonumber(responseText) then
-        print("\n^3----------------------------------------------------------------------------------^7")
-        print(script.." git version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!")
-		print("READ THE UPDATES.md to see if you have to make any changes!!")
+        print("[^3"..script.."^7] is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..version.."^7!\nupdate from https://github.com/MaDHouSe79/"..script.."")
+        print("^1----------------------------------------------------------------------------------^7")
+    elseif tonumber(version) > tonumber(responseText) then
+        print("^3----------------------------------------------------------------------------------^7")
+        print("[^3"..script.."^7] git version is: ^2"..responseText.."^7, installed version: ^1"..version.."^7!")
         print("^3----------------------------------------------------------------------------------^7")
     else
-        print("\n"..script.." is up to date. (^2"..curVersion.."^7)")
+        print("[^2"..script.."^7] is up to date (^2"..version.."^7) ")
     end
 end
 
