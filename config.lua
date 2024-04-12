15c511b71e6b96512221ebbb4ccea7422fd2f164
MaDHouSe <madhouse1979@gmail.com>
2024-04-12 16:06:26 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,8 +10,10 @@ Config.useRemoveBox = false -- true if you want to see the remove itembox popup 
 
 Config.CashItem = 'cash'
 Config.BlackmoneyItem = 'black_money'
+Config.CryptoItem = "crypto"
 
 Config.CashItems = {
     [Config.CashItem] = true,
     [Config.BlackmoneyItem] = true,
+    [Config.CryptoItem] = true,
 }
