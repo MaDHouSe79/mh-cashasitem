9b593b19624f0598f04242ae109a8325bd531565
MaDHouSe <madhouse1979@gmail.com>
2024-08-30 01:13:26 +0200

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -1,18 +1,12 @@
---[[ ===================================================== ]]--
---[[           MH Cash As Item Script by MaDHouSe          ]]--
---[[ ===================================================== ]]--
 local script = GetCurrentResourceName()
 
 local function checkVersion(err, responseText, headers)
     local curVersion = LoadResourceFile(script, "version")
     local version = string.gsub(curVersion, "%s+", "")
-    if responseText == nil then
-        print("[^1"..script.."^7] Check for script update ^1FAILED^7")
-        return
-    end
+    if responseText == nil then return print("[^1"..script.."^7] Check for script update ^1FAILED^7") end
     if version ~= responseText and tonumber(version) < tonumber(responseText) then
         print("^1----------------------------------------------------------------------------------^7")
-        print("[^3"..script.."^7] is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..version.."^7!\nupdate from https://github.com/MaDHouSe79/"..script.."")
+        print("[^3"..script.."^7] is outdated, latest version is: ^2"..responseText.."^7")
         print("^1----------------------------------------------------------------------------------^7")
     elseif tonumber(version) > tonumber(responseText) then
         print("^3----------------------------------------------------------------------------------^7")
@@ -23,6 +17,6 @@ local function checkVersion(err, responseText, headers)
     end
 end
 
-Citizen.CreateThread( function()
+CreateThread(function()
     PerformHttpRequest("https://raw.githubusercontent.com".."/MaDHouSe79/"..script.."/master/version", checkVersion, "GET")
 end)
