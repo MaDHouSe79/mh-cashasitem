045b1ab20f91e103dc805e90445f68cd5cd67035
MaDHouSe <madhouse1979@gmail.com>
2024-09-20 15:44:32 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ author 'MaDHouSe79'
 description 'MH CashAsItem - use cash black_money and crypto as item.'
 version '1.0'
 server_only 'yes'
-server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/commands.lua', 'server/update.lua'}
+server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
 dependencies {'qb-core', 'qb-inventory'} -- use this only if you use qb-inventory.
 --dependencies {'qb-core', 'ps-inventory'} -- use this only if you use ps-inventory.
 lua54 'yes'
