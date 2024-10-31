67b1f4931f951ec9a07c88bdc6c4c8fe00b84c63
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 20:56:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'MaDHouSe79'
 description 'MH CashAsItem - use cash black_money and crypto as item.'
-version '2.0'
+version '2.1.0'
 server_only 'yes'
 server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
 dependencies {'qb-core', 'qb-inventory'} -- use this only if you use qb-inventory.
