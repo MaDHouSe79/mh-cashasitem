45f32bdd6c9be233a393bf303792344d6d760e24
MaDHouSe <madhouse1979@gmail.com>
2025-03-28 12:00:42 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'MaDHouSe79'
 description 'MH CashAsItem - use cash black_money and crypto as an item.'
-version '1.0.1'
+version '1.0.2'
 server_only 'yes'
 server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
 lua54 'yes'
